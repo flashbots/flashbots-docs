379de39429e137f5f8535ab8cc548577be956557
ryry79261 <ryanjnoble@gmail.com>
2021-04-24 12:47:34 +0200

    Checking environment

--- scripts/setup-cname.js
+++ scripts/setup-cname.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 var fs = require('fs');
 
+console.log(process.env.CURRENT_BRANCH)
+console.log(process.env.DEPLOYMENT_BRANCH)
+
 fs.writeFile('./static/CNAME', process.env.TARGET_URL, function (err) {
   if (err) throw err;
   console.log('CNAME created for ', process.env.TARGET_URL);
