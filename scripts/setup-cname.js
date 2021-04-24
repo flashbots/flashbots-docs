2dbe0e68f1ead7bc179174519289bea9f331955a
ryry79261 <ryanjnoble@gmail.com>
2021-04-24 12:51:23 +0200

    Checkin secrets

--- scripts/setup-cname.js
+++ scripts/setup-cname.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 
 console.log(process.env.CURRENT_BRANCH)
 console.log(process.env.DEPLOYMENT_BRANCH)
+console.log(process.env.TESTING)
 
 fs.writeFile('./static/CNAME', process.env.TARGET_URL, function (err) {
   if (err) throw err;
