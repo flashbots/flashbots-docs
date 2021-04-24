d405d51d1110b922a6449c2c1fda45f421bf965c
Ryan Noble <ryanjnoble@gmail.com>
2021-04-24 13:19:11 +0200

    Update setup-cname.js

--- scripts/setup-cname.js
+++ scripts/setup-cname.js
@@ -2,10 +2,6 @@ require('dotenv').config()
 
 var fs = require('fs');
 
-console.log(process.env.CURRENT_BRANCH)
-console.log(process.env.DEPLOYMENT_BRANCH)
-console.log(process.env.TESTING)
-
 fs.writeFile('./static/CNAME', process.env.TARGET_URL, function (err) {
   if (err) throw err;
   console.log('CNAME created for ', process.env.TARGET_URL);
