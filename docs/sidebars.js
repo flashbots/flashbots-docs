b331bccb4bdb7d1f789188283044cf25cfaeea6d
Robert Miller <bert.c.miller@gmail.com>
2021-12-05 21:23:35 -0500

    feat: add page on ratelimiting for Flashbots Protect (#217)
    * feat: add page on ratelimiting
    
    * Update docs/flashbots-protect/rpc/ratelimiting.md
    
    Co-authored-by: Leo Arias <leo@flashbots.net>
    
    * Update docs/flashbots-protect/rpc/ratelimiting.md
    
    Co-authored-by: Leo Arias <leo@flashbots.net>
    
    * Update docs/flashbots-protect/rpc/ratelimiting.md
    
    Co-authored-by: Chris Hager <chris@linuxuser.at>
    
    Co-authored-by: Leo Arias <leo@flashbots.net>
    Co-authored-by: Chris Hager <chris@linuxuser.at>

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -107,6 +107,7 @@ module.exports = {
             'flashbots-protect/rpc/quick-start',
             'flashbots-protect/rpc/uncle-bandits',
             'flashbots-protect/rpc/status-api',
+            'flashbots-protect/rpc/ratelimiting',
             'flashbots-protect/rpc/cancellations',
             'flashbots-protect/rpc/releases',
           ]
