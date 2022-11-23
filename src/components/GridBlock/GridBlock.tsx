046130d475ee972113d327a19750b14e100c116c
RyRy79261 <ryanjnoble@gmail.com>
2022-11-23 10:06:17 +0200

    Changed card

--- src/components/GridBlock/GridBlock.tsx
+++ src/components/GridBlock/GridBlock.tsx
@@ -3,15 +3,30 @@ import styles from  './styles.module.scss';
 
 interface IGridBlock {
   children: ReactNode | ReactNode[]
+  symbol?: string
   title?: string
 }
 
-const GridBlock = ({ children, title }: IGridBlock) => {
+const GridBlock = ({ children, symbol, title }: IGridBlock) => {
   return (<div className={styles.root}>
+    <div className={styles.background}>
+      <div></div>
+      <div></div>
+      <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.23804 0.5H23.5309L1.23804 22.7929V0.5Z" fill="#DBDDE1" fill-opacity="0.25" stroke="#DBDDE1"/>
+      </svg>
+
+    </div>
     {
       title && (
         <div className={styles.title}>
-          { title }
+          {
+            symbol && <p>{ symbol }</p>
+          }
+          <span>
+            { title }
+          </span>
+          <hr />
         </div>
       )
     }
