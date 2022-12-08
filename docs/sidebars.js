93a565308c4dbfa13ca0ed5ac40833f90d456f4b
kailin <kailin@torvadata.com>
2022-12-07 21:09:09 -0600

    MEV Boost caps

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -100,29 +100,29 @@ module.exports = {
         'flashbots-mev-boost/introduction',
         {
           'Architecture Overview': [
-            'flashbots-mev-boost/architecture-overview/MEV-boost-specifications',
-            'flashbots-mev-boost/architecture-overview/MEV-boost-block-proposal',
-            'flashbots-mev-boost/architecture-overview/risks'
+            'flashbots-mev-boost/architecture-overview/MEV-Boost Specifications',
+            'flashbots-mev-boost/architecture-overview/MEV-Boost Block Proposal',
+            'flashbots-mev-boost/architecture-overview/Risks'
           ]
         },
 
-          'flashbots-mev-boost/block builders',
-          'flashbots-mev-boost/block proposers',
-          'flashbots-mev-boost/relays',
+          'flashbots-mev-boost/Block Builders',
+          'flashbots-mev-boost/Block Proposers',
+          'flashbots-mev-boost/Relays',
         {
           'Getting Started':[
-            'flashbots-mev-boost/getting-started/system-requirements',
-            'flashbots-mev-boost/getting-started/installation',
-            'flashbots-mev-boost/getting-started/usage',
+            'flashbots-mev-boost/getting-started/System Requirements',
+            'flashbots-mev-boost/getting-started/Installation',
+            'flashbots-mev-boost/getting-started/Usage',
           ]
         },
-          'flashbots-mev-boost/troubleshooting',
-          'flashbots-mev-boost/contributing',
-          'flashbots-mev-boost/security',
+          'flashbots-mev-boost/Troubleshooting',
+          'flashbots-mev-boost/Contributing',
+          'flashbots-mev-boost/Security',
           'flashbots-mev-boost/FAQ',
-          'flashbots-mev-boost/glossary',
-          'flashbots-mev-boost/resources',
-          'flashbots-mev-boost/community-tools',          
+          'flashbots-mev-boost/Glossary',
+          'flashbots-mev-boost/Resources',
+          'flashbots-mev-boost/Community Tools',          
       ],
     },
     {
