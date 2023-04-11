28e59fe7b244fe06946f648a8df8e425e47b2a9e
RyRy79261 <ryanjnoble@gmail.com>
2023-04-11 20:07:51 +0200

    Roughly added

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -3,13 +3,16 @@ import Layout from '@theme-original/Layout';
 import type LayoutType from '@theme/Layout';
 import type {WrapperProps} from '@docusaurus/types';
 import { Analytics } from '@vercel/analytics/react';
+import { MetaMaskProvider } from "metamask-react"
 type Props = WrapperProps<typeof LayoutType>;
 
 export default function LayoutWrapper(props: Props): JSX.Element {
   return (
     <>
-      <Layout {...props} />
-      <Analytics />
+      <MetaMaskProvider>
+        <Layout {...props} />
+        <Analytics />
+      </MetaMaskProvider>
     </>
   );
 }
