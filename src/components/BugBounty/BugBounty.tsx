3b277d5a2d887de57883339ed0df823f47659f27
deadpine <hey@deadpine.xyz>
2023-05-03 14:57:47 -0300

    Update styles, content and layout

--- src/components/BugBounty/BugBounty.tsx
+++ src/components/BugBounty/BugBounty.tsx
@@ -3,12 +3,15 @@ import styles from  './styles.module.scss';
 
 interface IBugBounty {
   children: ReactNode | ReactNode[]
-  amount?: string
   severity?: string
+  amount?: string
+  emoji?: string
+  example?: string
 }
 
-const BugBounty = ({ children, amount, severity }: IBugBounty) => {
+const BugBounty = ({ children, amount, severity, example, emoji }: IBugBounty) => {
   return (<div className={styles.root}>
+
     <div className={styles.background}>
       <div></div>
       <div></div>
@@ -17,21 +20,30 @@ const BugBounty = ({ children, amount, severity }: IBugBounty) => {
       </svg>
 
     </div>
-    {
-      severity && (
-        <div className={styles.severity}>
-          <h3>
-            { severity }
-          </h3>
-          {
-            amount && <p>Up to { amount } USD</p>
-          }
-          <hr />
-        </div>
-      )
-    }
+    
+    <div className={styles.header}>
+      <div className={styles.severity}>
+        <h3>{ severity } <span>{ emoji }</span></h3>
+        <b>Up to { amount } USD</b>
+      </div>
+      <a href="#">Submit { severity } risk bug
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M4.6977 13.3023C4.96131 13.5659 5.38869 13.5659 5.6523 13.3023L12.15 6.80459V11.925C12.15 12.2978 12.4522 12.6 12.825 12.6C13.1978 12.6 13.5 12.2978 13.5 11.925V5.175C13.5 4.80221 13.1978 4.5 12.825 4.5H6.075C5.70221 4.5 5.4 4.80221 5.4 5.175C5.4 5.54779 5.70221 5.85 6.075 5.85H11.1954L4.6977 12.3477C4.4341 12.6113 4.4341 13.0387 4.6977 13.3023Z"/>
+        </svg>
+      </a>
+    </div>
+
+    <hr />
+
     <div className={styles.content}>
-      {children}
+      <div>
+      <b>Criteria</b>
+        { children }
+      </div>
+      <div>
+        <b>Example</b>
+        <p>{ example }</p>
+      </div>
     </div>
   </div>)
 }
