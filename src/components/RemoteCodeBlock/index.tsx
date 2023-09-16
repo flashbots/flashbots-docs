854ce18edab6e2239b0324cf586d8232094b2c76
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2023-09-15 18:06:44 -0700

    add arb contract details

--- src/components/RemoteCodeBlock/index.tsx
+++ src/components/RemoteCodeBlock/index.tsx
@@ -1,29 +1,23 @@
 import React, {useState, useEffect} from "react"
-import CodeBlock from "@theme/CodeBlock"
+import CodeBlock, {Props as CodeProps} from "@theme/CodeBlock"
 
 /** Code block that fetches its code from a URL. */
-interface IRemoteCodeBlock {
-    /** Programming language of code at url. 
-     * [Supported languages](https://docusaurus.io/docs/markdown-features/code-blocks#supported-languages) */
-    language?: string
+interface IRemoteCodeBlock extends CodeProps {
     /** URL of code to be fetched. Must be a raw file. 
      * [Example](https://raw.githubusercontent.com/flashbots/pm/main/README.md) */
     url?: string
-    /** Title of code block, rendered as a bold span. If undefined, no title is rendered. */
-    title?: string
 }
 
-const RemoteCodeBlock = ({ language, url, title }: IRemoteCodeBlock) => {
+const RemoteCodeBlock = (props: IRemoteCodeBlock) => {
     const [content, setContent] = useState<string>()
     useEffect(() => {
-        fetch(url)
+        fetch(props.url)
             .then((response) => response.text())
             .then((text) => setContent(text))
-    }, [url])
+    }, [props.url])
     return (<>
-        {title && <span><strong>{title}</strong></span>}
-        <CodeBlock language={language}>
-            {content || `// Loading ${url} ...`}
+        <CodeBlock {...props}>
+            {content || `// Loading ${props.url} ...`}
         </CodeBlock>
     </>)
 }
