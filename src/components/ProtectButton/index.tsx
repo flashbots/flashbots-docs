caf24df7a3160c00fe5765767d63e62c9f648ed3
deadpine <hey@deadpine.xyz>
2023-10-26 13:51:49 -0300

    update RPC and fast mode containers

--- src/components/ProtectButton/index.tsx
+++ src/components/ProtectButton/index.tsx
@@ -140,7 +140,16 @@ function FlashbotsProtectButton(options: ProtectButtonOptions) {
   };
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center gap-2">
+      <div className="min-w-full max-w-full p-3 flex items-start gap-2 border-solid rounded-md border-slate-200">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 mt-0.5">
+          <path fill-rule="evenodd" d="M19.902 4.098a3.75 3.75 0 00-5.304 0l-4.5 4.5a3.75 3.75 0 001.035 6.037.75.75 0 01-.646 1.353 5.25 5.25 0 01-1.449-8.45l4.5-4.5a5.25 5.25 0 117.424 7.424l-1.757 1.757a.75.75 0 11-1.06-1.06l1.757-1.757a3.75 3.75 0 000-5.304zm-7.389 4.267a.75.75 0 011-.353 5.25 5.25 0 011.449 8.45l-4.5 4.5a5.25 5.25 0 11-7.424-7.424l1.757-1.757a.75.75 0 111.06 1.06l-1.757 1.757a3.75 3.75 0 105.304 5.304l4.5-4.5a3.75 3.75 0 00-1.035-6.037.75.75 0 01-.354-1z" clip-rule="evenodd" />
+        </svg>
+        <div className='max-w-[93%]'>
+          <p className='m-0 text-sm font-bold'>RPC URL</p>
+          <p className='m-0 text-sm break-words'>{rpcUrl.toString()}</p>
+        </div>
+      </div>
       <div className="flex justify-center">
         <button
           type="button"
@@ -148,10 +157,6 @@ function FlashbotsProtectButton(options: ProtectButtonOptions) {
           onClick={() => connectToProtect()}>
           {children}
         </button>
-      </div>
-      <div className={styles.rpcUrlContainer}>
-        <div className={styles.rpcUrlLabel}>RPC URL:</div>
-        <div className={styles.rpcUrl}>{rpcUrl.toString()}</div>
       </div>
     </div>
   );
--- src/components/ProtectButtonSelector/FastOptionCheckbox.tsx
+++ src/components/ProtectButtonSelector/FastOptionCheckbox.tsx
@@ -14,9 +14,15 @@ interface FastOptionCheckboxProps {
 
 function FastOptionCheckbox({ fastMode, setFastMode }: FastOptionCheckboxProps) {
   return (
-    <div className={styles.fastContainer}>
+    <div className="p-3 flex items-start gap-2 border-solid rounded-md border-slate-200">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 mt-0.5">
+        <path fill-rule="evenodd" d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z" clip-rule="evenodd" />
+      </svg>
+      <div className='max-w-[80%]'>
+        <p className='m-0 text-sm font-bold'>Fast mode</p>
+        <p className='m-0 text-sm'>Transactions are shared with all registered builders. A larger portion of the bundle value is sent to the validators</p>
+      </div>
       <Checkbox
-        label="Fast"
         id="fast"
         checked={fastMode}
         onChange={setFastMode}
