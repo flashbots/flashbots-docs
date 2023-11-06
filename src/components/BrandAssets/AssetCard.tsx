1d9cddc5d2341abcd12b0d710ef63402549584de
deadpine <hey@deadpine.xyz>
2023-11-06 15:41:30 -0300

    update color and padding

--- src/components/BrandAssets/AssetCard.tsx
+++ src/components/BrandAssets/AssetCard.tsx
@@ -19,7 +19,7 @@ function AssetCard({ title, cover, svg, png }: IAssetCard) {
   return (
     <div className="my-4 w-1/2 w-full">
       <img alt="" src={ cover } className="w-full h-56 object-cover border border-solid border-slate-500/25 rounded-xl"/>
-      <div className="w-full flex items-center gap-1 px-2 bg-gray-50/0 rounded-lg">
+      <div className="w-full flex items-center gap-1 bg-gray-50/0 rounded-lg">
         <p className="px-2 grow m-0">{ title }</p>
         <Download file={ svg } type="svg" />
         <Download file={ png } type="png" />
