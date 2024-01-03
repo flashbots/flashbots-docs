7af024ba808bd7c52f62dc1fe34dc37bb5dd4072
72L <siyiryanlee@gmail.com>
2024-01-03 20:12:57 +0000

    replace flashbots button

--- src/components/ProtectButtonSelector/index.tsx
+++ src/components/ProtectButtonSelector/index.tsx
@@ -4,119 +4,31 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import {useState} from 'react';
-import FlashbotsLogo from '@site/static/img/flashbots-logo.svg';
-import BrowserOnly from '@docusaurus/BrowserOnly';
-import FlashbotsProtectButton, {HintPreferences} from '../ProtectButton';
-import SimpleDropdown from '../SimpleDropdown';
-import BuilderOptions from './BuilderOptions';
-import {useSupportedBuilders} from '../mev-share/useSupportedBuilders';
-import FastOptionCheckbox from './FastOptionCheckbox';
-import MevShareHints from './MevShareHints';
 
-const defaultHintSelectors = {
-  calldata: false,
-  logs: false,
-  defaultLogs: false,
-  contractAddress: false,
-  functionSelector: false,
-};
+import { useEffect, useState } from 'react';
 
 export default function ProtectButtonSelector() {
-  const [hashOnly, setHashOnly] = useState(false);
-  const [advancedOptionsShown, setAdvancedOptionsShown] = useState(false);
-  const [fastMode, setFastMode] = useState(false);
-  // Initialize the state object with all hints set to false
-  const [hints, setHints] = useState(defaultHintSelectors);
-  const supportedBuilders = useSupportedBuilders().map(
-    (builder) => builder.name,
-  );
-  const [buildersSelection, setBuildersSelection] = useState(
-    Object.fromEntries(supportedBuilders.map((builder) => [builder, false])),
-  );
 
-  const selectedBuilders = Object.keys(buildersSelection).filter(
-    (builder) => buildersSelection[builder] === true,
-  );
+  const [theme, setTheme] = useState('light')
 
-  // Function to update a hint
-  const setHint = (label: string, value: boolean) => {
-    setHints((prevHints) => ({...prevHints, [label]: value}));
+  useEffect(() => {
+    const htmlElement = document.documentElement;
+    const handleThemeChange = () => {
+      const currentTheme = htmlElement.getAttribute('data-theme');
+      setTheme(currentTheme || 'light');
+    };
+    handleThemeChange();
+    const observer = new MutationObserver(handleThemeChange);
+    observer.observe(htmlElement, { attributes: true, attributeFilter: ['data-theme'] });
+  }, [])
 
-    // If the user selects any other hint, the "none" option should be
-    // deselected.
-    if (value === true) {
-      setHashOnly(false);
-    }
-  };
-
-  const hintsProcessed: HintPreferences = {
-    ...hints,
-    hash: hashOnly,
-  };
-
-  const onSetHashOnly = (val: boolean) => {
-    setHashOnly(val);
-    if (val === true) {
-      // We have to also clear all of the other hints if someone selects  hash
-      // only
-      setHints(defaultHintSelectors);
-    }
-  };
-
-  const setBuilder = (name: string) => {
-    setBuildersSelection((prevBuilders) => ({
-      ...prevBuilders,
-      [name]: !prevBuilders[name],
-    }));
-  };
-
-  return (
-    <div className="mx-auto max-w-md rounded-2xl border border-solid border-slate-200 bg-gray-50 p-2 text-gray-950">
-      <SimpleDropdown
-        header="Advanced options"
-        onClickHeader={() => {
-          setAdvancedOptionsShown(!advancedOptionsShown);
-        }}
-        isOpen={advancedOptionsShown}>
-        <SimpleDropdown.Body>
-          <div className="mb-2 flex flex-col gap-2 rounded-xl border-solid border-slate-200 bg-white p-4">
-            <div className="mb-3 flex">
-              <p className="m-0 grow text-2xl font-bold">
-                Flashbots Protect RPC
-              </p>
-              <FlashbotsLogo className="mx-1" />
-            </div>
-            <FastOptionCheckbox fastMode={fastMode} setFastMode={setFastMode} />
-            <BrowserOnly>
-              {() => (
-                <FlashbotsProtectButton
-                  hints={hintsProcessed}
-                  builders={selectedBuilders}
-                  fast={fastMode}>
-                  Connect Wallet to Protect
-                </FlashbotsProtectButton>
-              )}
-            </BrowserOnly>
-          </div>
-        </SimpleDropdown.Body>
-
-        <SimpleDropdown.HiddenBody>
-          <MevShareHints
-            hintLabels={Object.keys(defaultHintSelectors)}
-            hints={hints}
-            hashOnly={hashOnly}
-            setHint={setHint}
-            onSetHashOnly={onSetHashOnly}
-          />
-          <BuilderOptions
-            supportedBuilders={supportedBuilders}
-            buildersSelection={buildersSelection}
-            fastMode={fastMode}
-            setBuilder={setBuilder}
-          />
-        </SimpleDropdown.HiddenBody>
-      </SimpleDropdown>
-    </div>
-  );
+  return <iframe
+    title='Protect Button'
+    id="protect-button"
+    // replace with correct host
+    src={`https://flashbots-protect-72l.vercel.app/button?theme=${theme}`}
+    height="88" width="336"
+    className="flex dark:dark border-none rounded-lg hover:outline"
+    sandbox="allow-top-navigation-by-user-activation"
+  />
 }
